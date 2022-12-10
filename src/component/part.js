4c1f0e2e55d7acbb8fa2b4f3c25393d4c79e2252
Shufeng Wu <wu666@Shufengs-MacBook-Air.local>
2022-12-10 10:23:04 -0500

    i wonder what changed

--- src/component/part.js
+++ src/component/part.js
@@ -1,39 +1,10 @@
 import {useState} from 'react'
 import '../App.css';
-import axios from 'axios';
-import EditForm from './editform';
 
 const Recipe = (props)=>{
-
-    const [showUpdateForm, setShowUpdateForm] = useState(false)
-
-    const revealUpdate = () => {
-      {(showUpdateForm) ? setShowUpdateForm(false) : setShowUpdateForm(true)}  
-    }
-
-    
-
-    const deleteRecipe= (recipeData) => {
-      console.log(recipeData)
-      axios.delete(`http://localhost:3000/recipe/${recipeData._id}`).then(() => {
-        axios.get('http://localhost:3000/recipe/').then((response) => {
-          props.setRecipes(response.data)
-        })
-      })
-    
-    }
-
-
-
   return(
     <div>
-      {(showUpdateForm) ?
-      < EditForm recipe={props.recipe} revealUpdate={revealUpdate} />
-
-      :
-
-      <div>
-       <div className="card" style={{width:"20rem"}}>
+      <div className="card" style={{width:"20rem"}}>
         <img src={props.recipe.image} className="card-img-top"/>
         <h5 className="card-title">{props.recipe.name}</h5>
         <p>Time to prepare: {props.recipe.timeToPrepare} minutes</p>
@@ -45,11 +16,10 @@ const Recipe = (props)=>{
         </div>
         <div className="card-body">
           <a href={props.recipe.link} className="btn btn-info">link</a>
-          <button className="btn btn-primary" onClick={revealUpdate}>Edit</button>
-          <button className="btn btn-danger"  onClick={()=> {deleteRecipe(props.recipe)}}>Delete</button>
-         </div>
-       </div>
-      }
+          <button className="btn btn-primary">Edit</button>
+          <button className="btn btn-danger" >Delete</button>
+        </div>
+      </div>
     </div>
   )
 
