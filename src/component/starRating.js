482c3bbd595bf0441eb879d1967061e222886b3b
Joe Klein SEI <josephklein87@gmail.com>
2022-12-13 16:16:17 -0500

    created ability to save state of users rating as well as update schema with user rating

--- src/component/starRating.js
+++ src/component/starRating.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 // FOLLOWED THIS TUTORIAL TO CREATE COMPONENT 
 //https://dev.to/michaelburrows/create-a-custom-react-star-rating-component-5o6 //
@@ -9,22 +9,39 @@ const StarRating = (props) => {
     const [rating, setRating] = useState(0);
     const [hover, setHover] = useState(0);
 
-    const handleRatingRequest = () => {
-        axios
-        .put(`http://polar-forest-73812.herokuapp.com/recipe/rating/${props.recipe._id}`,
-        {
-            user: props.user.username,
-            rating: rating
-        })
-        .then(()=>{
-        axios
-        .get(props.lastSearch).then((res)=>{
-            props.setRecipes(res.data)
-        })
-
-        })
-
+    const handleRatingRequest = (index) => {
+        setRating(index)
     }
+    
+    useEffect(()=>{
+        axios
+            .put(`http://polar-forest-73812.herokuapp.com/recipe/rating/${props.recipe._id}`,
+            {
+                user: props.user.username,
+                rating: rating
+            })
+            .then(()=>{
+                axios
+                .get(props.lastSearch).then((res)=>{
+                    props.setRecipes(res.data)
+            })
+        })
+    }, [rating]
+    )
+
+    useEffect(()=>{
+        if (props.user.username) {
+        let userKey = props.user.username.toString()
+        console.log("this is the username: " + userKey)
+        setRating(props.recipe.ratings[userKey])
+        console.log("this is the data:" + props.recipe.ratings[userKey])
+        }
+    }, [])
+
+
+
+
+
     return (
       <div className="star-rating">
         {[...Array(5)].map((star, index) => {
@@ -34,7 +51,7 @@ const StarRating = (props) => {
               type="button"
               key={index}
               className={index <= (hover || rating) ? "on" : "off"}
-              onClick={() => {setRating(index); handleRatingRequest()}}
+              onClick={() => {handleRatingRequest(index)}}
               onMouseEnter={() => setHover(index)}
               onMouseLeave={() => setHover(rating)}
             >
